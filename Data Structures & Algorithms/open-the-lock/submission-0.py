56039a63f5e5256b7227a9f1d167f2a336605e0a
mahek gupta <mahekg819@gmail.com>
2026-07-07 16:26:37 +0530

    Add: open-the-lock - submission-0

---
+++ Data Structures & Algorithms/open-the-lock/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if "0000" in deadends:
+            return -1
+
+        def children(lock):
+            res = []
+            for i in range(4):
+                digit = str((int(lock[i]) + 1) % 10)
+                res.append(lock[:i] + digit + lock[i+1:])
+
+                digit = str((int(lock[i]) - 1 + 10) % 10)
+                res.append(lock[:i] + digit + lock[i+1:])
+            return res
+
+        q = deque()
+        q.append(("0000", 0))
+
+        visited = set(deadends)
+        visited.add("0000")
+
+        while q:
+            lock, turn = q.popleft()
+
+            if lock == target:
+                return turn
+
+            for child in children(lock):
+                if child not in visited:
+                    visited.add(child)
+                    q.append((child, turn + 1))
+
+        return -1
