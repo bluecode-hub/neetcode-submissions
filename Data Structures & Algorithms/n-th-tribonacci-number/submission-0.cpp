1ae73cce7264a1bbddbf7261f91f26e5b737cccc
mahek gupta <mahekg819@gmail.com>
2026-07-08 12:09:06 +0530

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int fibonacci(int n,vector<int>&dp){
+         if(n==0||n==1){
+            return n;
+        }
+        if(n==2){
+            return 1;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=fibonacci(n-1,dp)+fibonacci(n-2,dp)+fibonacci(n-3,dp);
+        return dp[n];
+
+    }
+    int tribonacci(int n) {
+        vector<int> dp(n+2,-1);
+        return fibonacci(n,dp);
+       
+
+        
+    }
+};
