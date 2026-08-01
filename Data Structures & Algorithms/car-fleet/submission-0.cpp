1e317f8380545072a2e59e56db529333434f6cbb
mahek gupta <mahekg819@gmail.com>
2026-08-01 13:13:26 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+
+        vector<pair<int,int>> cars;
+
+        for (int i = 0; i < position.size(); i++)
+            cars.push_back({position[i], speed[i]});
+
+        sort(cars.rbegin(), cars.rend());
+
+        int fleets = 0;
+        double lastTime = 0;
+
+        for (auto car : cars) {
+            double time = (double)(target - car.first) / car.second;
+
+            if (time > lastTime) {
+                fleets++;
+                lastTime = time;
+            }
+        }
+
+        return fleets;
+    }
+};
